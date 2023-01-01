3edf9d97eb6bb2f6db39c22c43459cb711be2614
tteckster <tteckster@gmail.com>
2023-01-01 18:10:04 -0500

    Update and rename zwave-js-ui-install.sh to zwave-js-ui-v5-install.sh

--- install/zwave-js-ui-v5-install.sh
+++ install/zwave-js-ui-v5-install.sh
@@ -19,7 +19,7 @@ set -o pipefail
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
 trap die ERR
-
+silent() { "$@" > /dev/null 2>&1; }
 function error_exit() {
   trap - ERR
   local reason="Unknown failure occurred."
@@ -77,22 +77,22 @@ alias die='EXIT=$? LINE=$LINENO error_exit'
 set -e
 
 msg_info "Updating Container OS"
-apt-get update &>/dev/null
-apt-get -y upgrade &>/dev/null
+$STD apt-get update
+$STD apt-get -y upgrade
 msg_ok "Updated Container OS"
 
 msg_info "Installing Dependencies"
-apt-get install -y curl &>/dev/null
-apt-get install -y sudo &>/dev/null
-apt-get install -y unzip &>/dev/null
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y unzip
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Z-Wave JS UI"
 mkdir /opt/zwave-js-ui
 cd /opt/zwave-js-ui
 RELEASE=$(curl -s https://api.github.com/repos/zwave-js/zwave-js-ui/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-wget https://github.com/zwave-js/zwave-js-ui/releases/download/${RELEASE}/zwave-js-ui-${RELEASE}-linux.zip &>/dev/null
-unzip zwave-js-ui-${RELEASE}-linux.zip &>/dev/null
+$STD wget https://github.com/zwave-js/zwave-js-ui/releases/download/${RELEASE}/zwave-js-ui-${RELEASE}-linux.zip
+$STD unzip zwave-js-ui-${RELEASE}-linux.zip
 msg_ok "Installed Z-Wave JS UI"
 
 msg_info "Creating Service"
@@ -108,7 +108,7 @@ ExecStart=/opt/zwave-js-ui/zwave-js-ui-linux
 [Install]
 WantedBy=multi-user.target" >$service_path
 systemctl start zwave-js-ui
-systemctl enable zwave-js-ui &>/dev/null
+$STD systemctl enable zwave-js-ui
 msg_ok "Created Service"
 
 PASS=$(grep -w "root" /etc/shadow | cut -b6)
@@ -135,6 +135,6 @@ fi
 
 msg_info "Cleaning up"
 rm zwave-js-ui-${RELEASE}-linux.zip
-apt-get autoremove >/dev/null
-apt-get autoclean >/dev/null
+$STD apt-get autoremove
+$STD apt-get autoclean
 msg_ok "Cleaned"
