32e76ce54bd0276fb34ce6447c0fb489d27c1241
tteckster <tteckster@gmail.com>
2023-01-03 11:10:10 -0500

    Update pihole-install.sh

--- install/pihole-install.sh
+++ install/pihole-install.sh
@@ -20,7 +20,7 @@ set -o pipefail
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
 trap die ERR
-
+silent() { "$@" > /dev/null 2>&1; }
 function error_exit() {
   trap - ERR
   local reason="Unknown failure occurred."
@@ -78,15 +78,15 @@ alias die='EXIT=$? LINE=$LINENO error_exit'
 set -e
 
 msg_info "Updating Container OS"
-apt-get update &>/dev/null
-apt-get -y upgrade &>/dev/null
+$STD apt-get update
+$STD apt-get -y upgrade
 msg_ok "Updated Container OS"
 
 msg_info "Installing Dependencies"
-apt-get install -y curl &>/dev/null
-apt-get install -y sudo &>/dev/null
-apt-get install -y ufw &>/dev/null
-apt-get install -y ntp &>/dev/null
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y ufw
+$STD apt-get install -y ntp
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Pi-hole"
@@ -107,7 +107,7 @@ WEBPASSWORD=$(openssl rand -base64 48)
 BLOCKING_ENABLED=true
 EOF
 
-curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended &>/dev/null
+$STD curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended
 msg_ok "Installed Pi-hole"
 
 PASS=$(grep -w "root" /etc/shadow | cut -b6)
@@ -133,6 +133,6 @@ if [[ "${SSH_ROOT}" == "yes" ]]; then
 fi
 
 msg_info "Cleaning up"
-apt-get autoremove >/dev/null
-apt-get autoclean >/dev/null
+$STD apt-get autoremove
+$STD apt-get autoclean
 msg_ok "Cleaned"
