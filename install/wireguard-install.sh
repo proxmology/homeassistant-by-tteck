1ca5b4f3e53671fb9011f827a848d448e60dfbf7
tteckster <tteckster@gmail.com>
2023-01-03 15:00:08 -0500

    Update wireguard-install.sh

--- install/wireguard-install.sh
+++ install/wireguard-install.sh
@@ -19,7 +19,7 @@ set -o pipefail
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
 trap die ERR
-
+silent() { "$@" > /dev/null 2>&1; }
 function error_exit() {
   trap - ERR
   local reason="Unknown failure occurred."
@@ -94,60 +94,27 @@ UNATTUPG=1
 EOF
 
 msg_info "Updating Container OS"
-apt-get update &>/dev/null
-apt-get -y upgrade &>/dev/null
+$STD apt-get update
+$STD apt-get -y upgrade
 msg_ok "Updated Container OS"
 
 msg_info "Installing Dependencies"
-apt-get install -y curl &>/dev/null
-apt-get install -y sudo &>/dev/null
-apt-get install -y gunicorn &>/dev/null
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y gunicorn
 msg_ok "Installed Dependencies"
 
-msg_info "Installing WireGuard (using pivpn.io)"
-curl -s -L https://install.pivpn.io >install.sh
-chmod +x install.sh
-./install.sh --unattended options.conf &>/dev/null
-msg_ok "Installed WireGuard"
-
 msg_info "Installing pip3"
-apt-get install python3-pip -y &>/dev/null
-pip install flask &>/dev/null
-pip install ifcfg &>/dev/null
-pip install flask_qrcode &>/dev/null
-pip install icmplib &>/dev/null
+$STD apt-get install -y python3-pip
+$STD pip install flask
+$STD pip install ifcfg
+$STD pip install flask_qrcode
+$STD pip install icmplib
 msg_ok "Installed pip3"
 
-msg_info "Installing WGDashboard"
-WGDREL=$(curl -s https://api.github.com/repos/donaldzou/WGDashboard/releases/latest |
-  grep "tag_name" |
-  awk '{print substr($2, 2, length($2)-3) }')
-
-git clone -b ${WGDREL} https://github.com/donaldzou/WGDashboard.git /etc/wgdashboard &>/dev/null
-cd /etc/wgdashboard/src
-sudo chmod u+x wgd.sh
-sudo ./wgd.sh install &>/dev/null
-sudo chmod -R 755 /etc/wireguard
-msg_ok "Installed WGDashboard"
-
-msg_info "Creating Service"
-service_path="/etc/systemd/system/wg-dashboard.service"
-echo "[Unit]
-After=netword.service
-
-[Service]
-WorkingDirectory=/etc/wgdashboard/src
-ExecStart=/usr/bin/python3 /etc/wgdashboard/src/dashboard.py
-Restart=always
-
-
-[Install]
-WantedBy=default.target" >$service_path
-sudo chmod 664 /etc/systemd/system/wg-dashboard.service
-sudo systemctl daemon-reload
-sudo systemctl enable wg-dashboard.service &>/dev/null
-sudo systemctl start wg-dashboard.service
-msg_ok "Created Service"
+msg_info "Installing WireGuard (using pivpn.io)"
+$STD bash <(curl -fsSL https://install.pivpn.io) --unattended options.conf
+msg_ok "Installed WireGuard"
 
 PASS=$(grep -w "root" /etc/shadow | cut -b6)
 if [[ $PASS != $ ]]; then
@@ -172,6 +139,6 @@ if [[ "${SSH_ROOT}" == "yes" ]]; then
 fi
 
 msg_info "Cleaning up"
-apt-get autoremove >/dev/null
-apt-get autoclean >/dev/null
+$STD apt-get autoremove
+$STD apt-get autoclean
 msg_ok "Cleaned"
