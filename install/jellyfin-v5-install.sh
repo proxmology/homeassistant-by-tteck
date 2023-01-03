0ac11e1722dfb910871e8bfd5d3445807a156946
tteckster <tteckster@gmail.com>
2023-01-03 17:01:28 -0500

    Update and rename jellyfin-install.sh to jellyfin-v5-install.sh

--- install/jellyfin-v5-install.sh
+++ install/jellyfin-v5-install.sh
@@ -19,7 +19,7 @@ set -o pipefail
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
 trap die ERR
-
+silent() { "$@" > /dev/null 2>&1; }
 function error_exit() {
   trap - ERR
   local reason="Unknown failure occurred."
@@ -77,23 +77,23 @@ alias die='EXIT=$? LINE=$LINENO error_exit'
 set -e
 
 msg_info "Updating Container OS"
-apt-get update &>/dev/null
-apt-get -y upgrade &>/dev/null
+$STD apt-get update
+$STD apt-get -y upgrade
 msg_ok "Updated Container OS"
 
 msg_info "Installing Dependencies"
-apt-get install -y curl &>/dev/null
-apt-get install -y sudo &>/dev/null
-apt-get install -y apt-transport-https &>/dev/null
-apt-get install -y software-properties-common &>/dev/null
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y apt-transport-https
+$STD apt-get install -y software-properties-common
 msg_ok "Installed Dependencies"
 
 if [[ -z "$(grep -w "100000" /proc/self/uid_map)" ]]; then
   msg_info "Setting Up Hardware Acceleration"
-  apt-get -y install \
+  $STD apt-get -y install \
     va-driver-all \
     ocl-icd-libopencl1 \
-    beignet-opencl-icd &>/dev/null
+    beignet-opencl-icd
 
   /bin/chgrp video /dev/dri
   /bin/chmod 755 /dev/dri
@@ -102,15 +102,17 @@ if [[ -z "$(grep -w "100000" /proc/self/uid_map)" ]]; then
 fi
 
 msg_info "Setting Up Jellyfin Repository"
-sudo add-apt-repository universe -y &>/dev/null
-wget -q -O - https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key | sudo apt-key add - &>/dev/null
-echo "deb [arch=$(dpkg --print-architecture)] https://repo.jellyfin.org/ubuntu $(lsb_release -c -s) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list &>/dev/null
+$STD add-apt-repository universe -y
+$STD apt-key add <(curl -fsSL https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key)
+cat <<EOF >/etc/apt/sources.list.d/jellyfin.list
+deb [arch=amd64] https://repo.jellyfin.org/ubuntu focal main
+EOF
 msg_ok "Set Up Jellyfin Repository"
 
 msg_info "Installing Jellyfin"
-apt-get update &>/dev/null
-sudo apt install jellyfin-server -y &>/dev/null
-apt install jellyfin-ffmpeg -y &>/dev/null
+$STD apt-get update
+$STD apt install jellyfin-server -y
+$STD apt install jellyfin-ffmpeg -y
 msg_ok "Installed Jellyfin"
 
 msg_info "Creating Service"
@@ -153,6 +155,6 @@ if [[ "${SSH_ROOT}" == "yes" ]]; then
 fi
 
 msg_info "Cleaning up"
-apt-get autoremove >/dev/null
-apt-get autoclean >/dev/null
+$STD apt-get autoremove
+$STD apt-get autoclean
 msg_ok "Cleaned"
