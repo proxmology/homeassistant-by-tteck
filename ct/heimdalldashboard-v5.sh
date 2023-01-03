a74cabc24eae8243d25b86b29a62abdb9a6b0c28
tteckster <tteckster@gmail.com>
2023-01-03 11:51:13 -0500

    Update heimdalldashboard-v5.sh

--- ct/heimdalldashboard-v5.sh
+++ ct/heimdalldashboard-v5.sh
@@ -81,7 +81,7 @@ if command -v pveversion >/dev/null 2>&1; then
   fi
 fi
 if ! command -v pveversion >/dev/null 2>&1; then
-  if [[ ! -f /etc/apt/sources.list.d/emqx_emqx.list ]]; then
+  if [[ ! -d /opt/Heimdall ]]; then
     msg_error "No ${APP} Installation Found!";
     exit 
   fi
@@ -310,10 +310,89 @@ function install_script() {
 function update_script() {
 clear
 header_info
-msg_info "Updating ${APP} LXC"
-apt-get update &>/dev/null
-apt-get -y upgrade &>/dev/null
-msg_ok "Updated ${APP} LXC"
+msg_info "Stopping ${APP}"
+systemctl disable heimdall.service &>/dev/null
+systemctl stop heimdall
+sleep 1
+msg_ok "Stopped ${APP}"
+
+msg_info "Backing up Data"
+if [ -d "/opt/Heimdall-2.4.6" ]; then
+  cp -R /opt/Heimdall-2.4.6/database database-backup
+  cp -R /opt/Heimdall-2.4.6/public public-backup
+elif [[ -d "/opt/Heimdall-2.4.7b" ]]; then
+  cp -R /opt/Heimdall-2.4.7b/database database-backup
+  cp -R /opt/Heimdall-2.4.7b/public public-backup
+elif [[ -d "/opt/Heimdall-2.4.8" ]]; then
+  cp -R /opt/Heimdall-2.4.8/database database-backup
+  cp -R /opt/Heimdall-2.4.8/public public-backup
+else
+  cp -R /opt/Heimdall/database database-backup
+  cp -R /opt/Heimdall/public public-backup
+fi
+sleep 1
+msg_ok "Backed up Data"
+
+RELEASE=$(curl -sX GET "https://api.github.com/repos/linuxserver/Heimdall/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]')
+msg_info "Updating Heimdall Dashboard to ${RELEASE}"
+curl --silent -o ${RELEASE}.tar.gz -L "https://github.com/linuxserver/Heimdall/archive/${RELEASE}.tar.gz" &>/dev/null
+tar xvzf ${RELEASE}.tar.gz &>/dev/null
+VER=$(curl -s https://api.github.com/repos/linuxserver/Heimdall/releases/latest |
+  grep "tag_name" |
+  awk '{print substr($2, 3, length($2)-4) }')
+
+if [ ! -d "/opt/Heimdall" ]; then
+  mv Heimdall-${VER} /opt/Heimdall
+else
+  cp -R Heimdall-${VER}/* /opt/Heimdall
+fi
+
+service_path="/etc/systemd/system/heimdall.service"
+echo "[Unit]
+Description=Heimdall
+After=network.target
+[Service]
+Restart=always
+RestartSec=5
+Type=simple
+User=root
+WorkingDirectory=/opt/Heimdall
+ExecStart="/usr/bin/php" artisan serve --port 7990 --host 0.0.0.0
+TimeoutStopSec=30
+[Install]
+WantedBy=multi-user.target" >$service_path
+msg_ok "Updated Heimdall Dashboard to ${RELEASE}"
+
+msg_info "Restoring Data"
+cp -R database-backup/* /opt/Heimdall/database
+cp -R public-backup/* /opt/Heimdall/public
+sleep 1
+msg_ok "Restored Data"
+
+msg_info "Cleanup"
+if [ -d "/opt/Heimdall-2.4.6" ]; then
+  rm -rf /opt/Heimdall-2.4.6
+  rm -rf /opt/v2.4.6.tar.gz
+elif [[ -d "/opt/Heimdall-2.4.7b" ]]; then
+  rm -rf /opt/Heimdall-2.4.7b
+  rm -rf /opt/v2.4.7b.tar.gz
+elif [[ -d "/opt/Heimdall-2.4.8" ]]; then
+  rm -rf /opt/Heimdall-2.4.8
+  rm -rf /opt/v2.4.8.tar.gz
+fi
+
+rm -rf ${RELEASE}.tar.gz
+rm -rf Heimdall-${VER}
+rm -rf public-backup
+rm -rf database-backup
+rm -rf Heimdall
+sleep 1
+msg_ok "Cleaned"
+
+msg_info "Starting ${APP}"
+systemctl enable --now heimdall.service &>/dev/null
+sleep 2
+msg_info "Started ${APP}"
 msg_ok "Update Successfull"
 exit
 }
