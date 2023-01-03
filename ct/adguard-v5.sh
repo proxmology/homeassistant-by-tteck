80e28aaa42cf9e3df0d9f800f7165ee630cb5cd3
tteckster <tteckster@gmail.com>
2023-01-03 09:26:01 -0500

    Update adguard-v5.sh

--- ct/adguard-v5.sh
+++ ct/adguard-v5.sh
@@ -19,6 +19,18 @@ var_os="debian"
 var_version="11"
 bash <(curl -fsSL https://raw.githubusercontent.com/tteck/Proxmox/v5/ct/debian-whip.sh)
 #bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/v5/ct/debian-whip.sh)"
+}
+function install_script() {
+  if (whiptail --title "SETTINGS" --yesno "Use Default Settings?" --no-button Advanced 10 58); then
+    header_info
+    echo -e "${BL}Using Default Settings${CL}"
+    default_settings
+  else
+    header_info
+    echo -e "${RD}Using Advanced Settings${CL}"
+    advanced_settings
+  fi
+}
 function update_script() {
 clear
 header_info
