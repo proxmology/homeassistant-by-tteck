50cd222926aaabcedfc27ad555b7a354629fc87c
tteckster <tteckster@gmail.com>
2023-01-04 11:56:06 -0500

    Update yunohost-v5.sh

--- ct/yunohost-v5.sh
+++ ct/yunohost-v5.sh
@@ -347,7 +347,9 @@ pct start $CTID
 msg_ok "Started LXC Container"
 lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/v5/install/$var_install.sh)" || exit
 IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
-pct set $CTID -description "# ${APP} ${var_version} LXC
+pct set $CTID -description "# ${APP} LXC
 ### https://tteck.github.io/Proxmox/
 <a href='https://ko-fi.com/D1D7EP4GF'><img src='https://img.shields.io/badge/☕-Buy me a coffee-red' /></a>"
 msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}/ ${CL}"
