951abb07111aa15f468fe2294ebce257db73df97
tteckster <tteckster@gmail.com>
2023-01-04 15:48:33 -0500

    Update nginxproxymanager-v5-install.sh

--- install/nginxproxymanager-v5-install.sh
+++ install/nginxproxymanager-v5-install.sh
@@ -136,8 +136,9 @@ RELEASE=$(curl -s https://api.github.com/repos/NginxProxyManager/nginx-proxy-man
   awk '{print substr($2, 3, length($2)-4) }')
 
 msg_info "Downloading Nginx Proxy Manager v${RELEASE}"
-$STD wget -q https://codeload.github.com/NginxProxyManager/nginx-proxy-manager/tar.gz/v${RELEASE} -O - | tar -xz
-cd nginx-proxy-manager-${RELEASE}
+$STD wget -q https://codeload.github.com/NginxProxyManager/nginx-proxy-manager/tar.gz/v${RELEASE}
+tar -xz v${RELEASE}
+cd ./nginx-proxy-manager-${RELEASE}
 msg_ok "Downloaded Nginx Proxy Manager v${RELEASE}"
 
 msg_info "Setting up Enviroment"
