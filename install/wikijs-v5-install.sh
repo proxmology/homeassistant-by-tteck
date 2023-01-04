a429011aa4022f551a79ad988392f91a925692d8
tteckster <tteckster@gmail.com>
2023-01-04 06:32:08 -0500

    Update and rename wikijs-install.sh to wikijs-v5-install.sh

--- install/wikijs-v5-install.sh
+++ install/wikijs-v5-install.sh
@@ -19,7 +19,7 @@ set -o pipefail
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
 trap die ERR
-
+silent() { "$@" > /dev/null 2>&1; }
 function error_exit() {
   trap - ERR
   local reason="Unknown failure occurred."
@@ -77,28 +77,28 @@ alias die='EXIT=$? LINE=$LINENO error_exit'
 set -e
 
 msg_info "Updating Container OS"
-apt-get update &>/dev/null
-apt-get -y upgrade &>/dev/null
+$STD apt-get update
+$STD apt-get -y upgrade
 msg_ok "Updated Container OS"
 
 msg_info "Installing Dependencies"
-apt-get install -y curl &>/dev/null
-apt-get install -y sudo &>/dev/null
-apt-get install -y git &>/dev/null
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y git
 msg_ok "Installed Dependencies"
 
 msg_info "Setting up Node.js Repository"
-curl -sL https://deb.nodesource.com/setup_16.x | bash - &>/dev/null
+$STD bash <(curl -fsSL https://deb.nodesource.com/setup_18.x)
 msg_ok "Set up Node.js Repository"
 
 msg_info "Installing Node.js"
-apt-get install -y nodejs &>/dev/null
+$STD apt-get install -y
 msg_ok "Installed Node.js"
 
 msg_info "Installing Wiki.js"
 mkdir -p /opt/wikijs
 cd /opt/wikijs
-wget https://github.com/Requarks/wiki/releases/latest/download/wiki-js.tar.gz &>/dev/null
+$STD wget https://github.com/Requarks/wiki/releases/latest/download/wiki-js.tar.gz
 tar xzf wiki-js.tar.gz
 rm wiki-js.tar.gz
 
@@ -113,7 +113,7 @@ logFormat: default
 dataPath: /opt/wikijs/data
 bodyParserLimit: 5mb
 EOF
-npm rebuild sqlite3 &>/dev/null
+$STD npm rebuild sqlite3
 msg_ok "Installed Wiki.js"
 
 msg_info "Creating Service"
@@ -133,7 +133,7 @@ WorkingDirectory=/opt/wikijs
 
 [Install]
 WantedBy=multi-user.target" >$service_path
-systemctl enable --now wikijs &>/dev/null
+$STD systemctl enable --now wikijs
 msg_ok "Created Service"
 
 PASS=$(grep -w "root" /etc/shadow | cut -b6)
@@ -158,6 +158,6 @@ if [[ "${SSH_ROOT}" == "yes" ]]; then
 fi
 
 msg_info "Cleaning up"
-apt-get autoremove >/dev/null
-apt-get autoclean >/dev/null
+$STD apt-get autoremove
+$STD apt-get autoclean
 msg_ok "Cleaned"
