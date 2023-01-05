072fea2da0772bc2cd532134ad41cbad92c0ac57
tteckster <tteckster@gmail.com>
2023-01-05 08:52:34 -0500

    Update and rename plex-install.sh to plex-v5-install.sh

--- install/plex-v5-install.sh
+++ install/plex-v5-install.sh
@@ -19,7 +19,7 @@ set -o pipefail
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
 trap die ERR
-
+silent() { "$@" > /dev/null 2>&1; }
 function error_exit() {
   trap - ERR
   local reason="Unknown failure occurred."
@@ -77,22 +77,22 @@ alias die='EXIT=$? LINE=$LINENO error_exit'
 set -e
 
 msg_info "Updating Container OS"
-apt-get update &>/dev/null
-apt-get -y upgrade &>/dev/null
+$STD apt-get update
+$STD apt-get -y upgrade
 msg_ok "Updated Container OS"
 
 msg_info "Installing Dependencies"
-apt-get install -y curl &>/dev/null
-apt-get install -y sudo &>/dev/null
-apt-get install -y gnupg &>/dev/null
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y gnupg
 msg_ok "Installed Dependencies"
 
 if [[ -z "$(grep -w "100000" /proc/self/uid_map)" ]]; then
   msg_info "Setting Up Hardware Acceleration"
-  apt-get -y install \
+  $STD apt-get -y install \
     va-driver-all \
     ocl-icd-libopencl1 \
-    beignet-opencl-icd &>/dev/null
+    beignet-opencl-icd
 
   /bin/chgrp video /dev/dri
   /bin/chmod 755 /dev/dri
@@ -101,13 +101,15 @@ if [[ -z "$(grep -w "100000" /proc/self/uid_map)" ]]; then
 fi
 
 msg_info "Setting Up Plex Media Server Repository"
-wget -q https://downloads.plex.tv/plex-keys/PlexSign.key -O - | sudo apt-key add - &>/dev/null
-echo "deb [arch=$(dpkg --print-architecture)] https://downloads.plex.tv/repo/deb/ public main" | tee /etc/apt/sources.list.d/plexmediaserver.list &>/dev/null
+$STD apt-key add <(curl -fsSL https://downloads.plex.tv/plex-keys/PlexSign.key)
+cat <<EOF >/etc/apt/sources.list.d/plexmediaserver.list
+deb [arch=amd64] https://downloads.plex.tv/repo/deb/ public main
+EOF
 msg_ok "Set Up Plex Media Server Repository"
 
 msg_info "Installing Plex Media Server"
-apt-get update &>/dev/null
-apt-get -o Dpkg::Options::="--force-confold" install -y plexmediaserver &>/dev/null
+$STD apt-get update
+$STD apt-get -o Dpkg::Options::="--force-confold" install -y plexmediaserver
 msg_ok "Installed Plex Media Server"
 
 PASS=$(grep -w "root" /etc/shadow | cut -b6)
@@ -132,6 +134,6 @@ if [[ "${SSH_ROOT}" == "yes" ]]; then
 fi
 
 msg_info "Cleaning up"
-apt-get autoremove >/dev/null
-apt-get autoclean >/dev/null
+$STD apt-get autoremove
+$STD apt-get autoclean
 msg_ok "Cleaned"
