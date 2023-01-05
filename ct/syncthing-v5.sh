860d1a7c1524610c592c1c2c4e75655dea84596e
tteckster <tteckster@gmail.com>
2023-01-05 10:55:16 -0500

    Update syncthing-v5.sh

--- ct/syncthing-v5.sh
+++ ct/syncthing-v5.sh
@@ -358,4 +358,4 @@ pct set $CTID -description "# ${APP} LXC
 <a href='https://ko-fi.com/D1D7EP4GF'><img src='https://img.shields.io/badge/☕-Buy me a coffee-red' /></a>"
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} should be reachable by going to the following URL.
-         ${BL}http://${IP}:8384 ${CL} (after initial start) \n"
+         ${BL}http://${IP}:8384 ${CL} \n"
