e5e991081f1999430c82e6ce76d9926ad650766c
tteckster <tteckster@gmail.com>
2023-01-05 08:43:12 -0500

    Update and rename postgresql-install.sh to postgresql-v5-install.sh

--- install/postgresql-v5-install.sh
+++ install/postgresql-v5-install.sh
@@ -19,7 +19,7 @@ set -o pipefail
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
 trap die ERR
-
+silent() { "$@" > /dev/null 2>&1; }
 function error_exit() {
   trap - ERR
   local reason="Unknown failure occurred."
@@ -77,25 +77,24 @@ alias die='EXIT=$? LINE=$LINENO error_exit'
 set -e
 
 msg_info "Updating Container OS"
-apt-get update &>/dev/null
-apt-get -y upgrade &>/dev/null
+$STD apt-get update
+$STD apt-get -y upgrade
 msg_ok "Updated Container OS"
 
 msg_info "Installing Dependencies"
-apt-get install -y curl &>/dev/null
-apt-get install -y sudo &>/dev/null
-apt-get install -y gnupg2 &>/dev/null
-apt-get install -y lsb-release &>/dev/null
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y gnupg
 msg_ok "Installed Dependencies"
 
 msg_info "Setting up PostgreSQL Repository"
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' &>/dev/null
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - &>/dev/null
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt bullseye-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+$STD apt-key add <(curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc)
 msg_ok "Setup PostgreSQL Repository"
 
 msg_info "Installing PostgreSQL"
-apt-get update &>/dev/null
-apt-get install -y postgresql &>/dev/null
+$STD apt-get update
+$STD apt-get install -y postgresql
 
 cat <<EOF >/etc/postgresql/15/main/pg_hba.conf
 # PostgreSQL Client Authentication Configuration File
@@ -209,9 +208,9 @@ fi
 
 if [[ $ADMINER == "Y" ]]; then
   msg_info "Installing Adminer"
-  sudo apt install adminer -y &>/dev/null
-  sudo a2enconf adminer &>/dev/null
-  sudo systemctl reload apache2 &>/dev/null
+  $STD apt install -y adminer
+  $STD sudo a2enconf adminer
+  $STD systemctl reload apache2
   msg_ok "Installed Adminer"
 fi
 
@@ -237,6 +236,6 @@ if [[ "${SSH_ROOT}" == "yes" ]]; then
 fi
 
 msg_info "Cleaning up"
-apt-get autoremove >/dev/null
-apt-get autoclean >/dev/null
+$STD apt-get autoremove
+$STD apt-get autoclean
 msg_ok "Cleaned"
