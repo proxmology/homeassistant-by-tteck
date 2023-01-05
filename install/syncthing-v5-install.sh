d86038b596ec46c4331f568dc014e1098ef908fa
tteckster <tteckster@gmail.com>
2023-01-05 10:36:01 -0500

    Update syncthing-v5-install.sh

--- install/syncthing-v5-install.sh
+++ install/syncthing-v5-install.sh
@@ -93,8 +93,9 @@ deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.sy
 EOF
 $STD apt-get update
 $STD apt-get install -y syncthing
-sed -i "{s/127.0.0.1:8384/0.0.1.0:8384/g}" /root/.config/syncthing/config.xml
 systemctl start syncthing@root.service
+sed -i "{s/127.0.0.1:8384/0.0.0.0:8384/g}" /root/.config/syncthing/config.xml
+systemctl restart syncthing@root.service
 $STD systemctl enable syncthing@root.service
 msg_ok "Installed Syncthing"
 
